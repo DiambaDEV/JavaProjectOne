659ac5777a882557852e1d16fb51f71f7614920b
Nikolas <faccionikolas@gmail.com>
2025-06-04 14:12:44 -0300

    Atualização geral

--- src/App.java
+++ src/App.java
@@ -1,3 +1,28 @@
+class Financiamento {
+
+    //ATRIBUTOS
+    double valorImovel;
+    int prazoFinanciamento;
+    double taxaJurosAnual;
+    //CONSTRUTOR
+    Financiamento(double valorDesejadoImovel, int prazoFinanciamentoAnos, double taxaJurosAnual){
+        this.valorImovel = valorDesejadoImovel;
+        this.prazoFinanciamento = prazoFinanciamentoAnos;
+        this.taxaJurosAnual = taxaJurosAnual;
+    }
+    //MÉTODOS
+    double calcularPagamentoMensal(){
+        return ( this.valorImovel/ (this.prazoFinanciamento * 12)) * (1 + (this.taxaJurosAnual / 12));
+    }
+    double calcularTotalPagamento(){
+        return this.calcularPagamentoMensal() * this.prazoFinanciamento * 12;
+    }
+}
+
+class InterfaceUsuario {
+
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
